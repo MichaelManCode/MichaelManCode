a3ecc1aa6cdc1208467d499f6d5388a4cc771a7c
Michael <104284296+MichaelManCode@users.noreply.github.com>
2022-06-11 17:58:08 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/* Author: Michael
+ *
+ * This program is a test for GitHub.
+ */
+
+public class HelloWorld {
+   public static void main(String[] theArgs) {
+      System.out.println("Hello World!");
+   }
+}
